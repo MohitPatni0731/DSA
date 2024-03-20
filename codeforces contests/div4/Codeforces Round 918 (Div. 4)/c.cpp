5ebe38e9472231809092541367fa93c2f4a0acbe
Mohit Patni <mohitpatni0731@gmail.com>
2024-03-20 21:30:01 +0530

    ,

---
+++ codeforces contests/div4/Codeforces Round 918 (Div. 4)/c.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main () {
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin>>n;
+        
+    }
+    
+}
