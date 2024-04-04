ab0604ddfe0251a934f63f0854b2afd752dff025
Mohit Patni <mohitpatni0731@gmail.com>
2024-04-04 12:48:10 +0530

    added middle_element, copy, remove_duplicate, swap_nodes, move_tail_to_head, reverse and is_palindrome implementation with their tests.

--- LinkedList cpp/linked_list.cpp
+++ LinkedList cpp/linked_list.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cassert>
 #include <sstream>
+#include <stack>
 using namespace std;
 
 class Node {
@@ -134,6 +135,106 @@ public:
         new_node->next = head;
         head = new_node;
     }
+
+    int middle_element() {
+        Node* current = head;
+        int count = 0;
+
+        while (current != nullptr) {
+            count += 1;
+            current = current->next;
+        }
+        
+        int middle = count / 2;
+        current = head;
+
+        for (int i = 0; i < middle; i++)
+        {
+            current = current->next;
+        }
+        int data = current->data;
+        return data;       
+    }
+
+    LinkedList* copy() {
+        LinkedList* copy_llist = new LinkedList();
+        Node* current = head;
+
+        while (current != nullptr) {
+            copy_llist->insert(current->data);
+            current = current->next;
+        }
+        return copy_llist;
+    }
+
+    void remove_duplicate() {
+        Node* current = head;
+        while (current != nullptr) {
+            Node* next_node = current;
+            next_node = current->next;
+            while (next_node != nullptr && next_node->data == current->data) {
+                next_node = next_node->next;
+            }
+            current->next = next_node;
+            current = next_node;
+        }
+    }
+
+    void swap_nodes(int x, int y) {
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->data == x) {
+                current->data = y;
+            } else if (current->data == y)
+            {
+                current->data = x;
+            }
+            current = current->next;
+        }
+    }
+
+    void move_head_to_tail() {
+        Node* current = head;
+        Node* previous = nullptr;
+        while (current->next != nullptr) {
+            previous = current;
+            current = current->next;
+        }
+        previous->next = nullptr;
+        current->next = head;
+        head = current;
+
+        return;
+    }
+
+    void reverse() {
+    std::stack<int> stack;
+    Node* current = head;
+
+    while (current != nullptr) {
+        stack.push(current->data);
+        current = current->next;
+    }
+
+    current = head;
+    
+    while (current != nullptr) {
+        current->data = stack.top();
+        stack.pop();
+        current = current->next;
+    }
+    }
+
+    bool is_plaindrome() {
+        LinkedList* reversedList = copy();
+        reversedList->reverse();
+
+        if (reversedList->head->data == head->data) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 void test_insert() {
@@ -220,6 +321,76 @@ void test_append() {
     assert(llist.head->next->data == 2);
     cout << "9: test_append() passed!" << endl; 
 }
+
+void test_middle_element() {
+    LinkedList llist;
+    llist.insert(1);
+    llist.insert(2);
+    llist.insert(3);
+
+    assert(llist.middle_element() == 2);
+    cout << "10: test_middle_element() passed!" << endl; 
+}
+
+void test_copy() {
+    LinkedList llist;
+    llist.insert(1);
+    llist.insert(2);
+    llist.insert(3);
+    LinkedList* llist1 = llist.copy();
+    assert(llist1->head->data == llist.head->data);
+    cout << "11: test_copy() passed!" << endl; 
+}
+
+void test_remove_duplicate() {
+    LinkedList llist;
+    llist.insert(2);
+    llist.insert(2);
+    llist.insert(3);
+    llist.remove_duplicate();
+    assert(llist.head->next->data == 3);
+    cout << "12: test_remove_duplicate() passed!" << endl; 
+}
+
+void test_swap_nodes() {
+    LinkedList llist;
+    llist.insert(1);
+    llist.insert(2);
+    llist.insert(3);
+    llist.swap_nodes(1,2);
+    assert(llist.head->data == 2);
+    cout << "13: test_swap_nodes() passed!" << endl; 
+}
+
+void test_move_head_to_tail() {
+    LinkedList llist;
+    llist.insert(1);
+    llist.insert(2);
+    llist.insert(3);
+    llist.move_head_to_tail();
+    assert(llist.head->data == 3);
+    cout << "14: test_move_head_to_tail() passed!" << endl; 
+}
+
+void test_reverse() {
+    LinkedList llist;
+    llist.insert(1);
+    llist.insert(2);
+    llist.insert(4);
+    llist.reverse();
+    assert(llist.head->data == 4);
+    cout << "15: test_reverse() passed!" << endl; 
+}
+
+void test_is_palindrome() {
+    LinkedList llist;
+    llist.insert(1);
+    llist.insert(2);
+    llist.insert(4);
+    assert(llist.is_plaindrome() == false);
+    cout << "16: test_is_palindrome() passed!" << endl; 
+}
+
 int main() {
     test_insert();
     test_print();
@@ -230,6 +401,13 @@ int main() {
     test_index();
     test_pop();
     test_append();
+    test_middle_element();
+    test_copy();
+    test_remove_duplicate();
+    test_swap_nodes();
+    test_move_head_to_tail();
+    test_reverse();
+    test_is_palindrome();
 
     // Just printing the LinkedList
     LinkedList llist;
