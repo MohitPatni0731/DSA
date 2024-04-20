0adfd2bdbc97dfde19ae3844cdd917f21dbe0e8f
Mohit Patni <mohitpatni0731@gmail.com>
2024-04-20 14:27:17 +0530

    added implementation of add_edge function, also added outline of other functions which I am going to implement, remove the self.node from the __init__ function

--- Graph/main.py
+++ Graph/main.py
@@ -1,21 +1,72 @@
 class Graph:
-    def __init__(self, Nodes) -> None:
+    def __init__(self) -> None:
         """
         Time - O(1)
         Space - O(1)
         """
-        self.nodes = Nodes
         self.adjacency_list = {}
+
+    def add_edge(self, node_1, node_2) -> None:
+        """
+        Time - O(1)
+        Space - O(1)
+        """
+        if node_1 not in self.adjacency_list:
+            self.adjacency_list[node_1] = []
+        if node_2 not in self.adjacency_list:
+            self.adjacency_list[node_2] = []
+        self.adjacency_list[node_1].append(node_2)
+        self.adjacency_list[node_2].append(node_1)
+
+    def add_vertex(self, vertex) -> None:
+        """
+        Time -
+        Space -
+        """
+
+    def is_empty(self) -> None:
+        """
+        Time -
+        Space -
+        """
+
+    def size_of_vertices(self) -> None:
+        """
+        Time -
+        Space -
+        """
+
+    def size_of_edges(self) -> None:
+        """
+        Time -
+        Space -
+        """
+
+    def remove_edge(self, edge) -> None:
+        """
+        Time -
+        Space -
+        """
+
+    def remove_vertex(self, vertex) -> None:
+        """
+        Time -
+        Space -
+        """
 
     def print_graph(self) -> None:
         """
         Time - O(N)
         Space - O(1)
         """
-        for node in self.nodes:
-            print(node, " -> ", self.adjacency_list.get(node))
+        for node in self.adjacency_list:
+            print(node, " -> ", self.adjacency_list[node])
 
 
-nodes = ["A", "B"]
-graph = Graph(nodes)
+graph = Graph()
+graph.add_edge("A", "B")
+graph.add_edge("B", "C")
+graph.add_edge("B", "D")
+graph.add_edge("D", "E")
+graph.add_edge("E", "A")
 graph.print_graph()
