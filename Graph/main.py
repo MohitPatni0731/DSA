9ef063657923c2af4e75bdd3eb3245a62e61c592
Mohit Patni <mohitpatni0731@gmail.com>
2024-04-21 20:23:05 +0530

    improved issue and added implementation of size of vertex and remove vertex function

--- Graph/main.py
+++ Graph/main.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class Graph:
     def __init__(self) -> None:
         """
@@ -6,20 +9,21 @@ class Graph:
         """
         self.adjacency_list = {}
 
-    def add_edge(self, node_1, node_2) -> None:
+    def add_edge(self, node1, node2) -> None:
         """
         Time - O(1)
         Space - O(1)
         """
-        self.add_vertext(node_1)
-        self.add_vertex(node_2)
-          
-        self.adjacency_list[node_1].append(node_2)
-        self.adjacency_list[node_2].append(node_1)
+        self._add_vertex(node1)
+        self._add_vertex(node2)
+
+        self.adjacency_list[node1].append(node2)
+        self.adjacency_list[node2].append(node1)
+
     def _add_vertex(self, vertex) -> None:
         """
-        Time -
-        Space -
+        Time - O(1)
+        Space - O(1)
         """
         if vertex not in self.adjacency_list:
             self.adjacency_list[vertex] = []
@@ -33,9 +37,10 @@ class Graph:
 
     def size_of_vertices(self) -> None:
         """
-        Time -
-        Space -
+        Time - O(1)
+        Space - O(1)
         """
+        print(len(self.adjacency_list))
 
     def size_of_edges(self) -> None:
         """
@@ -47,20 +52,22 @@ class Graph:
             length += len(self.adjacency_list[edge])
         print(length // 2)
 
-    def remove_edge(self, node_1, node_2) -> None:
+    def remove_edge(self, node1: Any, node2: Any) -> None:
         """
         Time - O(1)
         Space - O(1)
         """
-        if node_1 in self.adjacency_list and node_2 in self.adjacency_list:
-            self.adjacency_list[node_1].remove(node_2)
-            self.adjacency_list[node_2].remove(node_1)
+        if node1 in self.adjacency_list and node2 in self.adjacency_list:
+            self.adjacency_list[node1].remove(node2)
+            self.adjacency_list[node2].remove(node1)
 
     def remove_vertex(self, vertex) -> None:
         """
-        Time -
-        Space -
+        Time - O(1)
+        Space - O(1)
         """
+        if vertex in self.adjacency_list:
+            del self.adjacency_list[vertex]
 
     def print_graph(self) -> None:
         """
@@ -79,5 +86,6 @@ graph.size_of_edges()
 graph.is_empty()
 graph.remove_edge("A", "B")
 graph.print_graph()
-graph.add_vertex("F")
+graph._add_vertex("F")
 graph.print_graph()
+graph.size_of_vertices()
