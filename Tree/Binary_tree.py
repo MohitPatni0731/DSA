2c2d1728a03d4c4967ae463681407b457590b7b6
Mohit Patni <mohitpatni0731@gmail.com>
2024-04-27 16:09:25 +0530

    implemented pre, in and post order

--- Tree/Binary_tree.py
+++ Tree/Binary_tree.py
@@ -8,13 +8,17 @@ class BinaryTree:
         if self.left_child is None:
             self.left_child = BinaryTree(data)
         else:
-            raise ValueError("Left child already exists.")
+            t = BinaryTree(data)
+            t.left_child = self.left_child
+            self.left_child = t
 
     def insert_right(self, data):
         if self.right_child is None:
             self.right_child = BinaryTree(data)
         else:
-            raise ValueError("Rihgt child already exists.")
+            t = BinaryTree(data)
+            t.right_child = self.right_child
+            self.right_child = t
 
     def get_right(self):
         return self.right_child
@@ -27,3 +31,27 @@ class BinaryTree:
 
     def get_root(self):
         return self.data
+
+    def preorder(self):
+        if self:
+            print(self.get_root())
+            if self.get_left():
+                self.get_left().preorder()
+            if self.get_right():
+                self.get_right().preorder()
+
+    def inorder(self):
+        if self:
+            if self.get_left():
+                self.get_left().inorder()
+            print(self.get_root())
+            if self.get_right():
+                self.get_right().inorder()
+
+    def postorder(self):
+        if self:
+            if self.get_left():
+                self.get_left().postorder()
+            if self.get_right():
+                self.get_right().postorder()
+            print(self.get_root())
