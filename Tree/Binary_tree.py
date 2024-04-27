b87e732d615382528cd7e3dca8d7697365741361
Mohit Patni <mohitpatni0731@gmail.com>
2024-04-27 22:19:02 +0530

    added typing, time and space complexity of each function

--- Tree/Binary_tree.py
+++ Tree/Binary_tree.py
@@ -1,36 +1,72 @@
+from typing import Any
+from typing import List
+
+
 class BinaryTree:
     def __init__(self, data) -> None:
+        """
+        Time - O(1)
+        Space - O(1)
+        """
         self.data = data
         self.left_child = None
         self.right_child = None
 
-    def insert_left(self, data):
+    def insert_left(self, data: Any):
+        """
+        Time - O(1)
+        Space - O(1)
+        """
         if self.left_child is None:
             self.left_child = BinaryTree(data)
             return self.left_child
         else:
             raise ValueError("Left child already exists.")
 
-    def insert_right(self, data):
+    def insert_right(self, data: Any) -> None:
+        """
+        Time - O(1)
+        Space - O(1)
+        """
         if self.right_child is None:
             self.right_child = BinaryTree(data)
             return self.right_child
         else:
             raise ValueError("Right child already exists.")
 
-    def get_right(self):
+    def get_right(self) -> None:
+        """
+        Time - O(N)
+        Space - O(N)
+        """
         return self.right_child
 
-    def get_left(self):
+    def get_left(self) -> None:
+        """
+        Time - O(1)
+        Space - O(1)
+        """
         return self.left_child
 
-    def set_root(self, obj):
+    def set_root(self, obj) -> None:
+        """
+        Time - O(1)
+        Space - O(1)
+        """
         self.data = obj
 
-    def get_root(self):
+    def get_root(self) -> Any:
+        """
+        Time - O(1)
+        Space - O(1)
+        """
         return self.data
 
-    def preorder(self):
+    def preorder(self) -> List[Any]:
+        """
+        Time - O(N)
+        Space - O(N)
+        """
         preorder = []
         if self:
             preorder.append(self.get_root())
@@ -40,7 +76,11 @@ class BinaryTree:
                 preorder += self.get_right().preorder()
         return preorder
 
-    def inorder(self):
+    def inorder(self) -> List[Any]:
+        """
+        Time - O(N)
+        Space - O(N)
+        """
         inorder = []
         if self:
             if self.get_left():
@@ -50,7 +90,11 @@ class BinaryTree:
                 inorder += self.get_right().inorder()
         return inorder
 
-    def postorder(self):
+    def postorder(self) -> List[Any]:
+        """
+        Time - O(N)
+        Space - O(N)
+        """
         postorder = []
         if self:
             if self.get_left():
@@ -59,10 +103,3 @@ class BinaryTree:
                 postorder += self.get_right().postorder()
             postorder.append(self.get_root())
         return postorder
-
-
-r = BinaryTree(1)
-r.insert_left(2)
-r.insert_right(3)
-r.get_left().insert_left(4)
-print(r.preorder())
