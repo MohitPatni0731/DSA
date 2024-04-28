8df87b79a7970aaf952f84d7cfbaf1e7bd675366
Mohit Patni <mohitpatni0731@gmail.com>
2024-04-28 17:13:01 +0530

    added test case for insert method

--- Tree/test_Binary_search_tree.py
+++ Tree/test_Binary_search_tree.py
@@ -23,3 +23,36 @@ def test_inorder(setup_tree_for_testing):
 
 def test_postorder(setup_tree_for_testing):
     assert setup_tree_for_testing.postorder() == [5, 7, 6, 3, 2, 1]
+
+
+def test_insert_duplicate():
+    tree = BST(5)
+    tree.insert(5)
+    assert tree.preorder() == [5, 5]
+    assert tree.inorder() == [5, 5]
+    assert tree.postorder() == [5, 5]
+
+
+def test_insert_left():
+    tree = BST(5)
+    tree.insert(3)
+    assert tree.preorder() == [5, 3]
+    assert tree.inorder() == [3, 5]
+    assert tree.postorder() == [3, 5]
+
+
+def test_insert_right():
+    tree = BST(5)
+    tree.insert(7)
+    assert tree.preorder() == [5, 7]
+    assert tree.inorder() == [5, 7]
+    assert tree.postorder() == [7, 5]
+
+
+def test_insert_left_and_right():
+    tree = BST(5)
+    tree.insert(3)
+    tree.insert(7)
+    assert tree.preorder() == [5, 3, 7]
+    assert tree.inorder() == [3, 5, 7]
+    assert tree.postorder() == [3, 7, 5]
