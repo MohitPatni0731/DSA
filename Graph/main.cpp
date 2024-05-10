9029ddbcb77dd41ee5c58521c8e7c04d5ea9de6a
Mohit Patni <mohitpatni0731@gmail.com>
2024-05-10 17:15:18 +0530

    .

---
+++ Graph/main.cpp
@@ -0,0 +1,13 @@
+// Graph implementation using adjacency matrix
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Graph {
+private:
+    int vertices;
+    vector<vector<int>> adjacency_matrix;
+
+public:
+    
+}
