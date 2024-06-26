90f89cd03d139854505a2bfd17b6b76704a68e46
Mohit Patni <mohitpatni0731@gmail.com>
2024-06-26 21:50:06 +0530

    added heapify, insert and print function

--- Priority Queue/priority_queue.py
+++ Priority Queue/priority_queue.py
@@ -26,3 +26,41 @@ class PriorityQueue:
         Space - O(1)
         """
         return (2 * i) + 2
+
+    def peek(self) -> int:
+        """
+        Time - O(1)
+        Space - O(1)
+        """
+        return self.queue[0] if self.queue else None
+
+    def isEmpty(self) -> bool:
+        """
+        Time - O(1)
+        Space - O(1)
+        """
+        return len(self.queue) == 0
+
+    def heapify(self, i) -> None:
+        """
+        Time - O(N)
+        Space - O(N)
+        """
+        while i > 0 and self.queue[self.parent(i)] < self.queue[i]:
+            p = self.parent(i)
+            self.queue[i], self.queue[p] = self.queue[p], self.queue[i]
+
+    def insert(self, item) -> int:
+        """
+        Time - O(N)
+        Space - O(N)
+        """
+        self.queue.append(item)
+        self.heapifyUp(self.queue)
+
+    def print_queue(self) -> None:
+        """
+        Time - O(N)
+        Space - O(N)
+        """
+        return self.queue
